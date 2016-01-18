6bcb4b18334c02f9ae1300d56e6663438d23d13c
Luke Church <lukechurch@google.com>
2016-01-18 15:40:53 +0000

    AST Feature extractors for compeltion

---
+++ lib/logging_utils.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:logging/logging.dart' as logging;
+
+logging.Logger _logger;
+bool _setup = false;
+
+info(String message) => _logger.info(message);
+trace(String message) => _logger.finer(message);
+debug(String message) => _logger.fine(message);
+alert(String message) => _logger.shout(message);
+
+perf(String name, int ms) => _logger.fine("PERF: $name : $ms");
+
+/// Setup log streaming to the right place for local and remote deployment.
+setupLogging([String loggerName = "common"]) {
+  _logger = new logging.Logger(loggerName);
+
+  // TODO(lukechurch): Add support for container logs
+  _setupLocalLogging();
+}
+
+/// Setup log streaming to stdOut.
+_setupLocalLogging() {
+  if (_setup) return;
+  _setup = true;
+  
+  // Setup the logging
+  logging.hierarchicalLoggingEnabled = false;
+  logging.Logger.root.level = logging.Level.ALL;
+  logging.Logger.root.onRecord.listen((logging.LogRecord rec) {
+    print('${rec.loggerName}: ${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
